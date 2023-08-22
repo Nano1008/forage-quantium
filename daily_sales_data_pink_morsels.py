1403963321028dc0eaedaafe3f5b63fcb869d399
sinan <nanossn1008@gmail.com>
2023-08-22 15:06:28 +0800

    visualize daily sales of pink morsels with a line chart

--- daily_sales_data_pink_morsels.py
+++ daily_sales_data_pink_morsels.py
@@ -4,12 +4,14 @@ import plotly.express as px
 
 
 df = pd.read_csv("daily_sales_data_pink_morsels.csv")
+# sort by date
+df = df.sort_values(by="date")
 
 app = Dash(__name__)
 
 app.layout = html.Div([
     html.H2(children="Daily Sales of Pink Morsels", style={"textAlign": "center"}),
-    dcc.Graph(figure=px.line(df, x="date", y="sales")),
+    dcc.Graph(figure=px.line(df, x="date", y="sales", title="Pink Morsel Sales")),
 
 ])
 
